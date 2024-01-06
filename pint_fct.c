88cb6927629043654e69953c3f71db8ab40165b4
Spacefox95 <nathan.raynal@gmail.com>
2024-01-06 02:45:30 -0800

    New_error_msg

--- pint_fct.c
+++ pint_fct.c
@@ -10,7 +10,7 @@ void pint_fct(stack_t **stack, unsigned int line_number)
 {
 	if (stack == NULL || *stack == NULL)
 	{
-		fprintf(stderr, "L %d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
--- swap_fct.c
+++ swap_fct.c
@@ -17,7 +17,7 @@ void swap_fct(stack_t **stack, unsigned int line_number)
 
 	if ((*stack)->next == NULL)
 	{
-		fprintf(stderr, "L %d : can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%d : can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
