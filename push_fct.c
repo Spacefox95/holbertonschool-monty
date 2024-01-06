c44a8df94d7bdf4117ef6cdd06eaef91a770de8a
Spacefox95 <nathan.raynal@gmail.com>
2024-01-06 02:55:08 -0800

    Betty

--- push_fct.c
+++ push_fct.c
@@ -12,7 +12,7 @@ void push_fct(stack_t **stack, unsigned int line_number)
 	char *line;
 
 	line = get_file_line(filename, line_number);
-	if (split_until_int((const char*) line) == NULL)
+	if (split_until_int((const char *) line) == NULL)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
@@ -23,7 +23,7 @@ void push_fct(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	new_node->n = atoi(split_until_int((const char*) line));
+	new_node->n = atoi(split_until_int((const char *) line));
 	new_node->prev = NULL;
 	new_node->next = *stack;
 	if (*stack != NULL)
